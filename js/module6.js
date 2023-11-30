67c37f730ae391b87fee3d17b8f063d184316e00
iudinant <131783168+iudinant@users.noreply.github.com>
2023-11-30 18:35:27 +0100

    Update module6.js

--- js/module6.js
+++ js/module6.js
@@ -276,3 +276,744 @@
 // console.log(dog.hasOwnProperty("legs")); // false
 
 
+// const objC = { c: "objC prop" };
+
+// const objB = Object.create(objC);
+// objB.b = "objB prop";
+
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
+
+// console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+// console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+// console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// // console.log(parent.heritage);
+// console.log(child.surname);
+// console.log(ancestor.isPrototypeOf("parent"));
+// console.log(parent.isPrototypeOf("child"));
+// console.log(ancestor.hasOwnProperty("surname"));
+
+
+// const objB = {
+// 	b: "objB prop"
+// };
+
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
+
+// console.log(objA);
+
+
+// Object Oriented Programming ________________________________________________________________________
+
+//classes________
+
+// class User {
+//   // Тіло класу
+// }
+
+// const mango = new User();
+// console.log(mango); // {}
+
+// const poly = new User();
+// console.log(poly); // {}
+
+
+// class User {
+//   constructor(name, email) {
+// 		console.log(name, email);
+//   }
+// }
+
+// const mango = new User("Mango", "mango@mail.com"); // "Mango mango@mail.com"
+// console.log(mango); // {}
+
+
+// class User {
+//   constructor(name, email) {
+//     // Ініціалізація властивостей екземпляра
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User("Mango", "mango@mail.com");
+// console.log(mango); // { name: 'Mango', email: 'mango@mail.com' }
+
+// const poly = new User("Poly", "poly@mail.com");
+// console.log(poly); // { name: 'Poly', email: 'poly@mail.com' }
+
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car("Audi", "Q3", 36000);
+// console.log(audi);
+
+// const bmw = new Car("BMW", "X5", 58900);
+// console.log(bmw); 
+
+// const nissan = new Car("Nissan", "Murano", 31700);
+// console.log(nissan); 
+
+
+// class User {
+//   constructor(params) {
+//     this.name = params.name;
+//     this.email = params.email;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
+// console.log(mango); // { name: "Mango", email: "mango@mail.com" }
+
+
+// Oскільки конструктор — це звичайна функція, до його параметра можна застосувати деструктуризацію.
+
+
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
+// console.log(mango); // { name: "Mango", email: "mango@mail.com" }
+
+
+// class Car {
+//   constructor(param) {
+//     this.brand = param.brand;
+//     this.model = param.model;
+//     this.price = param.price;
+//   }
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+// console.log(new Car({ brand: "BMW", model: "X5", price: 58900 }));
+// console.log(new Car({ brand: "Nissan", model: "Murano", price: 31700 }));
+
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// const mango = new User({ 
+// 	name: "Mango", 
+// 	email: "mango@mail.com" 
+// });
+
+// console.log(mango.getEmail()); // "mango@mail.com"
+
+// mango.changeEmail("new@mail.com");
+
+// console.log(mango.getEmail()); // "new@mail.com"
+
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getPrice(){
+//     return this.price;
+//   }
+//   changePrice(newPrice){
+//     this.price = newPrice;
+//   }
+// }
+
+
+// class User {
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// console.log(User.prototype); // {constructor: ƒ, getEmail: ƒ, changeEmail: ƒ}
+
+
+// class User {
+//   // Необов'язкове оголошення публічних властивостей
+//   name;
+//   // Обов'язкове оголошення приватних властивостей
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
+// console.log(mango.name); // "Mango"
+// console.log(mango.#email); // Виникне помилка, це приватна властивість
+
+
+
+// Для того щоб отримати або змінити значення приватної властивості використовуються публічні методи.
+
+// class User {
+//   name;
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
+
+// console.log(mango.getEmail()); // "mango@mail.com"
+// mango.changeEmail("mango@supermail.com");
+// console.log(mango.getEmail()); // "mango@supermail.com"
+
+
+// class Car {
+//     #brand;
+//     model;
+//     price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   getPrice() {
+//     return this.price;
+//   }
+
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+
+//   getBrand(){
+//     return this.#brand;
+//   }
+
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+// }
+
+// console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
+
+
+// class User {
+//   name;
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Публічний метод для отримання електронної пошти
+//   getEmail() {
+//     return this.#email;
+//   }
+
+//   // Публічний метод для зміни електронної пошти
+//   changeEmail(newEmail) {
+//     if (this.#validateEmail(newEmail)) {
+//       this.#email = newEmail;
+//     } else {
+//       console.log('Invalid email format');
+//     }
+//   }
+
+//   // Приватний метод для валідації електронної пошти
+//   #validateEmail(email) {
+//     return email.includes('@');
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   email: 'mango@mail.com',
+// });
+
+// // Спробуємо змінити електронну пошту
+// mango.changeEmail('newmail.com'); // "Invalid email format"
+// mango.changeEmail('new@mail.com');
+// console.log(mango.getEmail()); // "new@mail.com"
+
+// Прямий виклик приватного методу ззовні призведе до помилки
+//mango.#validateEmail('test'); // Помилка
+
+
+// getters and setters ____________________________________________________
+
+// Геттери і сеттери — це спеціальний синтаксис оголошення методів для взаємодії з властивостями. 
+// Геттер і сеттер імітують звичайну публічну властивість класу,
+//     але дозволяють взаємодіяти з іншими властивостями зручнішим способом.
+
+// class User {
+//   #email;
+
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.#email = email;
+//   }
+
+//   // Геттер email
+//   get email() {
+//     return this.#email;
+//   }
+
+//   // Сеттер email
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// const mango = new User({ 
+// 	name: "Mango", 
+// 	email: "mango@mail.com" 
+// });
+
+// console.log(mango.email); // mango@mail.com
+
+// mango.email = "mango@supermail.com";
+
+// console.log(mango.email); // mango@supermail.com
+
+// set email(newEmail) {
+//   if(newEmail === "") {
+//     console.log("Помилка! Пошта не може бути порожнім рядком!");
+//     return;
+//   }
+
+//   this.#email = newEmail;
+// }
+
+
+
+// class Car {
+//     #brand;
+//     #model;
+//     #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
+
+
+//Статичні властивості
+
+// class MyClass {
+// 	static myProp = "value";
+// }
+
+// console.log(MyClass.myProp); // "value"
+
+// У екземпляра немає доступу до статичних властивостей класу.
+
+// class MyClass {
+// 	static myProp = "value";
+// }
+
+// const inst = new MyClass();
+// console.log(inst.myProp); // undefined
+
+
+// class User {
+//   static roles = {
+//     admin: "admin",
+//     editor: "editor",
+// 		basic: "basic"
+//   };
+
+//   #email;
+//   #role;
+
+//   constructor({ email, role = User.roles.basic }) {
+//     this.#email = email;
+//     this.#role = role;
+//   }
+
+//   get role() {
+//     return this.#role;
+//   }
+
+//   set role(newRole) {
+//     this.#role = newRole;
+//   }
+// }
+
+// const mango = new User({
+//   email: "mango@mail.com",
+//   role: User.roles.admin,
+// });
+
+// console.log(mango.role); // "admin"
+// mango.role = User.roles.editor;
+// console.log(mango.role); // "editor"
+
+
+// class Car {
+//     #price;
+//     #maxPrice;
+
+//     static maxPrice = 50000;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//     this.#maxPrice = 50000;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice > this.#maxPrice) {
+        
+//     } else {
+//         this.#price = newPrice;
+//     }
+    
+//   }
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+//_____________________________________________________________
+// 	class MyClass {
+// 		static myMethod() {
+// 			console.log("A static method");
+// 	}
+// }
+
+// MyClass.myMethod(); // "A static method"
+
+// class User {
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     return User.#takenEmails.includes(email);
+//   }
+
+//   #email;
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     User.#takenEmails.push(email);
+//   }
+// }
+
+// const mango = new User({ email: "mango@mail.com" });
+
+// console.log(User.isEmailTaken("poly@mail.com")); // false
+// console.log(User.isEmailTaken("mango@mail.com")); // true
+
+// Особливість статичних методів 
+// Під час їх виклику ключове слово this посилається на сам клас. 
+// Це означає, що статичний метод може отримати доступ до статичних властивостей класу,
+//     але не до властивостей екземпляра.Це логічно,
+//         адже статичні методи викликає сам клас, а не його екземпляри.
+
+
+
+// class Car {
+//     static #maxPrice = 50000;
+    
+//     static checkPrice(price) {
+//         if (price > this.#maxPrice) {
+//             return "Error! Price exceeds the maximum";
+//         } else {
+//             return "Success! Price is within acceptable limits";
+//         }
+//     }
+
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+    
+
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price));
+// console.log(Car.checkPrice(bmw.price)); 
+
+// console.log(Car.checkPrice(36000));
+// console.log(Car.checkPrice(64000) );
+
+
+//extends_____________________________________________________
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+// 	// Тіло класу ContentEditor
+// }
+
+// const editor = new ContentEditor("mango@mail.com");
+// console.log(editor); // { #email: "mango@mail.com" }
+// console.log(editor.email); // "mango@mail.com"
+
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//     static role = { BASIC: "basic", SUPERUSER: "superuser" };
+// }
+
+// console.log(Admin.role.BASIC);
+// console.log(Admin.role.SUPERUSER);
+
+
+// class User {
+//   #email;
+
+//   constructor(email) {
+//     this.#email = email;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+
+// class ContentEditor extends User {
+//   constructor({ email, posts }) {
+//     super(email); // Виклик конструктора батьківського класу User
+//     this.posts = posts;
+//   }
+// }
+
+// const editor = new ContentEditor({ 
+// 	email: "mango@mail.com", 
+// 	posts: [] 
+// });
+// console.log(editor); // { #email: "mango@mail.com", posts: [] }
+// console.log(editor.email); // "mango@mail.com"
+
+
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//     static access = "superuser";
+
+//   static role = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+    
+//     constructor({ email, access }) {
+//         super(email);
+//         this.access = access;
+//     }
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   access: Admin.role.SUPERUSER,
+// });
+
+// console.log(mango.email); 
+// console.log(mango.access); 
+// console.log(Admin.role.BASIC);
+// console.log(Admin.role.SUPERUSER);
+
+
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   static role = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor({ email, access }) {
+//     super(email);
+//     this.access = access;
+//   }
+//      blacklistedEmails = [];
+
+//     blacklist(email) {
+//         this.blacklistedEmails.push(email);
+//     }
+
+//     isBlacklisted(email) {
+//         return this.blacklistedEmails.includes(email);
+//     }
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   access: Admin.role.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.access); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
